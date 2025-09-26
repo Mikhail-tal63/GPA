1eabf41c92365b460e4223217049f33dc959296e
SA-BRY <sabryelbahloul@gmail.com>
2025-09-26 07:43:34 +0200

    fixing marge conflicts

--- server/routes/semesterRouter.js
+++ server/routes/semesterRouter.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { getSemesters, createSemester, deleteSemester } from "../controller/SemesterController.js";
+import protactRoute from "../middleware/ProtactRoate.js";
+
 const router = express.Router();
 
 
 
-router.get("/", getSemesters);
-router.post("/create", createSemester);
-router.delete("/delete/:id", deleteSemester);
+router.get("/getSemesters", protactRoute, getSemesters);
+router.post("/create", protactRoute, createSemester);
+router.delete("/delete/:id", protactRoute, deleteSemester);
 
 export default router;
